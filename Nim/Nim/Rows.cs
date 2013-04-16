1a613cc4d746beacf155aed4421f21743440e363
androyd09 <stevelight16@gmail.com>
2013-04-16 09:59:33 -0600

    Added back rows.cs

---
+++ Nim/Nim/Rows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nim
+{
+    class Rows
+    {
+        public int row1 { get; set; }
+        public int row2 { get; set; }
+        public int row3 { get; set; }
+
+        public Rows(int _row1, int _row2, int _row3)
+        {
+            row1 = _row1;
+            row2 = _row2;
+            row3 = _row3;
+        }
+    }
+}
